3cbbf1e9cc1ae513b6c59add7d290c0fa9e5ec55
Arun M <154700060+nvisblee@users.noreply.github.com>
2025-11-05 16:05:23 +0000

    Trying to add the methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,56 @@
 public class MyLinkedList
 {
+    private ListNode head;
+    private int size;
+
+    public MyLinkedList()
+    {
+        head = null;
+        size = 0;
+    } // end constructor
+
+    public void add(Object value)
+    {
+        ListNode newNode = new ListNode(value, head);
+        head = newNode;
+        size++;
+    } // end add
+
+    public Object get(int index)
+    {
+        if (index < 0 || index >= size)
+        {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        ListNode current = head;
+        for (int i = 0; i < index; i++)
+        {
+            current = current.getNext();
+        }
+        return current.getValue();
+    } // end get
+
+    public boolean isEmpty() { 
+        if(size == 0 || head == newNode) {
+            return true;
+        }
+        return false;
+
+
+    }
+
+    public int size()
+    {
+        return size;
+    } // end size
+
+
+    public String toString() { 
+    return "Head:" + head.getValue() + "  Heads Next Value:" + head.getNext().getValue(); + "Index:" + size;
+    }
+
+    
+
 
 
 }
